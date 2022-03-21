424e6beb1f69e77a5f35d8fba888fb963b47ef0d
Shiva <shanshiva@outlook.com>
2022-03-21 16:45:26 +0530

    Modified Lottery Contract with Chainlink VRF

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,15 +2,37 @@
 
 pragma solidity ^0.8.11;
 
-contract Lottery {
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+contract Lottery is VRFConsumerBase {
     address public owner;
     address payable[] public players;
     uint public lotteryId;
     mapping (uint => address payable) public lotteryHistory;
 
-    constructor() {
-        owner = msg.sender;
-        lotteryId = 1;
+    bytes32 internal keyHash; // identifies which Chainlink oracle to use
+    uint internal fee;        // fee to get random number
+    uint public randomResult;
+
+    constructor()
+        VRFConsumerBase(
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF coordinator
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709  // LINK token address
+        ) {
+            keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+            fee = 0.1 * 10 ** 18;    // 0.1 LINK
+
+            owner = msg.sender;
+            lotteryId = 1;
+        }
+
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK in contract");
+        return requestRandomness(keyHash, fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint randomness) internal override {
+        randomResult = randomness;
     }
 
     function getWinnerByLottery(uint lottery) public view returns (address payable) {
@@ -26,24 +48,24 @@ contract Lottery {
     }
 
     function enter() public payable {
-        require(msg.value > .01 ether);
+        require(msg.value > .001 ether);
 
         // address of player entering lottery
         players.push(payable(msg.sender));
     }
 
-    function getRandomNumber() public view returns (uint) {
-        return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
+    function pickWinner() public onlyowner {
+        getRandomNumber();
     }
 
-    function pickWinner() public onlyowner {
-        uint index = getRandomNumber() % players.length;
+    function payWinner() public onlyowner {
+        require(randomResult > 0, "Must have a source of randomness before choosing winner");
+        uint index = randomResult % players.length;
         players[index].transfer(address(this).balance);
 
         lotteryHistory[lotteryId] = players[index];
         lotteryId++;
         
-
         // reset the state of the contract
         players = new address payable[](0);
     }
